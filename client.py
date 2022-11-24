c70fa354efe0d701e26edb60b37264058f736778
Evg <zeka.tkach1@gmail.com>
2022-11-24 03:30:19 +0300

    added simple server, client, gen_cert

---
+++ client.py
@@ -0,0 +1,57 @@
+import socket
+import threading
+import sys
+import ssl
+
+
+class Client:
+    def __init__(self, cert_file_name):
+        self.context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+        self.context.verify_mode = ssl.CERT_REQUIRED
+        self.context.check_hostname = True
+        self.context.load_verify_locations(cert_file_name)
+
+    #Wait for incoming data from server
+    def receive(self, socket, signal):
+        while signal:
+            try:
+                data = socket.recv(32)
+                print(str(data.decode("utf-8")))
+            except:
+                print('You have been disconnected from the server.')
+                signal = False
+                break
+
+    def connect(self, host='localhost', port=10023, server_hostname='localhost'):
+        #Attempt connection to server
+        try:
+            self.socket = self.context.wrap_socket(socket.socket(socket.AF_INET), server_hostname=server_hostname)
+            self.socket.connect((host, port))
+        except:
+            print('Could not make a connection to the server.\nPlease verify the authenticity of the certificate.')
+            sys.exit(0)
+
+    def start(self):
+        #Create new thread to wait for data
+        receiveThread = threading.Thread(target = self.receive, args = (self.socket, True))
+        receiveThread.start()
+        self.send_message()
+
+    def send_message(self):
+            #Send data to server
+            while True:
+                try:
+                    message = input()
+                    self.socket.sendall(str.encode(message))
+                except KeyboardInterrupt:
+                    print('You have logged out of the server.')
+                    self.socket.close()
+                    break
+
+
+if __name__ == '__main__':
+    host = 'localhost'
+    port = 10023
+    client = Client('.certificates/server.crt')
+    client.connect(host, port, 'localhost')
+    client.start()
---
+++ server.py
@@ -0,0 +1,72 @@
+import socket
+import threading
+import ssl
+
+#Constants for holding information about connections
+connections = []
+total_connections = 0
+
+class Client(threading.Thread):
+    def __init__(self, socket, address, _id, name, signal):
+        threading.Thread.__init__(self)
+        self.socket = socket
+        self.address = address
+        self.id = _id
+        self.name = name
+        self.signal = signal
+    
+    def __str__(self):
+        return f'{str(self.id)} {str(self.address)}'
+    
+    def run(self):
+        while self.signal:
+            try:
+                data = self.socket.recv(32)
+            except:
+                print(f'Client {str(self.address)} has disconnected')
+                self.signal = False
+                connections.remove(self)
+                break
+            if data != "":
+                print(f'ID {str(self.id)}: {str(data.decode("utf-8"))}')
+                for client in connections:
+                    if client.id != self.id:
+                        client.socket.sendall(data)
+
+class Server:
+    def __init__(self, cert_filename, key_filename):
+        print('Starting the server')
+        self.context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+        self.context.load_cert_chain(certfile=cert_filename, keyfile=key_filename)
+        print('[SYSTEM] The server is running')
+
+    def start(self, host='localhost', port=10023):
+        #Create new server socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.bind((host, port))
+        sock.listen(5)
+        self.thread_connection(sock)
+
+    #Wait for new connections
+    def newConnections(self, socket):
+        while True:
+            sock, address = socket.accept()
+            try:
+                wrapped_socket = self.context.wrap_socket(sock, server_side=True)
+                global total_connections
+                connections.append(Client(wrapped_socket, address, total_connections, "Name", True))
+                connections[len(connections) - 1].start()
+                print(f'New connection at ID {str(connections[len(connections) - 1])}')
+                total_connections += 1
+            except:
+                print(f'[ALERT] The client {address} has an unknown SSL certificate')
+
+    def thread_connection(self, server_socket):
+        #Create new thread to wait for connections
+        newConnectionsThread = threading.Thread(target = self.newConnections, args = (server_socket,))
+        newConnectionsThread.start()
+
+
+if __name__ == '__main__':
+    server = Server("certificates/server.crt", "certificates/server.key")
+    server.start('localhost', 10023)
